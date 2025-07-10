002b4e9a4be2ebdfa219a49aeb1fd662f12d27f0
Simon Aničić <simon.anicic@gmail.com>
2025-07-10 19:56:43 +0200

    add validation to hardwareDTO

--- src/main/java/hr/algebra/java/hardware/dto/HardwareDTO.java
+++ src/main/java/hr/algebra/java/hardware/dto/HardwareDTO.java
@@ -1,6 +1,8 @@
 package hr.algebra.java.hardware.dto;
 
 import hr.algebra.java.hardware.domain.HardwareType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class HardwareDTO {
+
+    @NotBlank(message = "Naziv hardwarea je obavezan!")
     private String hardwareNaziv;
+    @NotBlank(message = "Šifra hardwarea je obavezna!")
     private String hardwareSifra;
+    @PositiveOrZero(message = "Cijena mora biti 0 ili veca!")
     private double hardwareCijena;
+    @NotBlank(message = "Tip hardwarea je obavezan!")
     private HardwareType hardwareTip;
+    @PositiveOrZero(message = "Kolicina na stanju mora biti 0 ili veca!")
     private int hardwareKolicinaNaStanju;
 }
